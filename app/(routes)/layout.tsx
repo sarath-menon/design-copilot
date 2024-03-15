7c809bade880205248a081dcd50f4c2163715275
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-15 18:26:17 +0100

    added right menu

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,11 +1,18 @@
 import DesignCopilot from "@/components/copilot/design-copilot";
+import { RightMenu } from "@/components/menus/right-menu";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className=" ">
       {children}
 
-      <DesignCopilot />
+      <div className="absolute top-0 right-0">
+        <RightMenu />
+      </div>
+
+      <div className="absolute top-0 left-0">
+        <DesignCopilot />
+      </div>
     </main>
   );
 }
