4a3bc4c9c2dc4ca30f5a2388317d6b3cc0e2062d
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-15 22:57:54 +0100

    added section, better id names

--- app/(routes)/(landing)/page.tsx
+++ app/(routes)/(landing)/page.tsx
@@ -2,23 +2,26 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <div
-        draggable="true"
-        className="flex flex-col items-center space-y-8 border p-8 hover:cursor-pointer hover:border-gray-500"
-      >
-        <h1
-          id="selva_h1"
-          className="text-6xl font-semibold tracking-tight"
+    <main id="main1" className="flex min-h-screen flex-col items-center p-24">
+      <section>
+        <div
+          id="div1"
           draggable="true"
+          className="flex flex-col items-center space-y-8 border p-8 hover:cursor-pointer hover:border-gray-500"
         >
-          Selva
-        </h1>
+          <h1
+            id="selva_h1"
+            className="text-6xl font-semibold tracking-tight"
+            draggable="true"
+          >
+            Selva
+          </h1>
 
-        <Button id="selva_btn" draggable="true">
-          Click me
-        </Button>
-      </div>
+          <Button id="selva_btn" draggable="true">
+            Click me
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
