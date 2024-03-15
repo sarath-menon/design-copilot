11a276ce318abf42e9c12a85f4df4a82f8852c60
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-15 22:32:46 +0100

    made div draggable

--- app/(routes)/(landing)/page.tsx
+++ app/(routes)/(landing)/page.tsx
@@ -3,7 +3,10 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <div className="flex flex-col items-center space-y-8">
+      <div
+        draggable="true"
+        className="flex flex-col items-center space-y-8 border p-8 hover:cursor-pointer hover:border-gray-500"
+      >
         <h1
           id="selva_h1"
           className="text-6xl font-semibold tracking-tight"
