7eea996e3a9aa7b90086f7a1c29969fdcd36836b
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-16 11:09:19 +0100

    added text in the middle

--- components/ui/wireframe.tsx
+++ components/ui/wireframe.tsx
@@ -5,15 +5,25 @@ interface WireframeProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
   height: string;
   width: string;
+  text: string;
 }
 
-function Wireframe({ className, height, width, ...props }: WireframeProps) {
+function Wireframe({
+  className,
+  height,
+  width,
+  text,
+  ...props
+}: WireframeProps) {
   return (
-    <div className={cn("", className)} {...props}>
+    <div className={cn("relative", className)} {...props}>
       <Skeleton
         className="rounded-md animate-none"
         style={{ width: `${width}px`, height: `${height}px` }}
       />
+      <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl">
+        {text}
+      </span>
     </div>
   );
 }
