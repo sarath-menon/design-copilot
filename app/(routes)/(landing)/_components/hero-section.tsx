8f52f6985fc6daa849e82ce3203c093b3e3027a2
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-16 07:39:24 +0100

    added xterm terminal

--- app/(routes)/(landing)/_components/hero-section.tsx
+++ app/(routes)/(landing)/_components/hero-section.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { Terminal } from "@xterm/xterm";
+import { useEffect, useRef } from "react";
+import "@xterm/xterm/css/xterm.css";
 
 export default function HeroSection() {
+  const terminalRef = useRef(null);
+
+  useEffect(() => {
+    const terminal = new Terminal();
+    if (terminalRef.current) {
+      terminal.open(terminalRef.current);
+      terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
+    }
+    return () => terminal.dispose();
+  }, []);
+
   return (
     <section
       id="section1"
-      draggable="true"
-      className="border hover:cursor-pointer hover:border-gray-500"
+      //   draggable="true"
+      //   className="border hover:cursor-pointer hover:border-gray-500"
     >
       <div id="div1" className="flex flex-col items-center space-y-8 p-8">
         <h1
@@ -19,6 +35,8 @@ export default function HeroSection() {
         <Button id="selva_btn" draggable="true">
           Click me
         </Button>
+
+        <div id="terminal" ref={terminalRef}></div>
       </div>
     </section>
   );
