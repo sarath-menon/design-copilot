a1fb35ea542629d512c8f04e543a3fc292e383e1
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-16 09:24:44 +0100

    cleanup

--- app/(routes)/(landing)/_components/hero-section.tsx
+++ app/(routes)/(landing)/_components/hero-section.tsx
@@ -1,14 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
 // import XtermTerminal from "@/components/xterm-terminal";
+import { promises as fs } from "fs";
 
 import dynamic from "next/dynamic";
 
-const DynamicXtermTerminal = dynamic(
-  () => import("@/components/xterm-terminal"),
-  {
-    loading: () => <p>Loading...</p>,
-  }
-);
+// const DynamicXtermTerminal = dynamic(
+//   () => import("@/components/xterm-terminal"),
+//   {
+//     loading: () => <p>Loading...</p>,
+//   }
+// );
+
+// function createFile() {
+//   fs.writeFile("mynewfile3.txt", "Hello content!", function (err) {
+//     if (err) throw err;
+//     console.log("Saved!");
+//   });
+// }
 
 export default function HeroSection() {
   return (
@@ -17,7 +26,11 @@ export default function HeroSection() {
       //   draggable="true"
       //   className="border hover:cursor-pointer hover:border-gray-500"
     >
-      <div id="div1" className="flex flex-col items-center space-y-8 p-8">
+      <div
+        id="div1"
+        draggable="true"
+        className="flex flex-col items-center space-y-8 p-8"
+      >
         <h1
           id="selva_h1"
           className="text-6xl font-semibold tracking-tight"
@@ -29,9 +42,11 @@ export default function HeroSection() {
         <Button id="selva_btn" draggable="true">
           Click me
         </Button>
+
+        <Calendar />
       </div>
 
-      <DynamicXtermTerminal />
+      {/* <DynamicXtermTerminal /> */}
     </section>
   );
 }
