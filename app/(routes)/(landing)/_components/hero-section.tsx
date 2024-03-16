a1311db6694931af74a8829845cd021a4dc09c8e
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-16 07:49:09 +0100

    cleanup

--- app/(routes)/(landing)/_components/hero-section.tsx
+++ app/(routes)/(landing)/_components/hero-section.tsx
@@ -1,22 +1,16 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
-import { Terminal } from "@xterm/xterm";
-import { useEffect, useRef } from "react";
-import "@xterm/xterm/css/xterm.css";
+// import XtermTerminal from "@/components/xterm-terminal";
+
+import dynamic from "next/dynamic";
+
+const DynamicXtermTerminal = dynamic(
+  () => import("@/components/xterm-terminal"),
+  {
+    loading: () => <p>Loading...</p>,
+  }
+);
 
 export default function HeroSection() {
-  const terminalRef = useRef(null);
-
-  useEffect(() => {
-    const terminal = new Terminal();
-    if (terminalRef.current) {
-      terminal.open(terminalRef.current);
-      terminal.write("Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ");
-    }
-    return () => terminal.dispose();
-  }, []);
-
   return (
     <section
       id="section1"
@@ -35,9 +29,9 @@ export default function HeroSection() {
         <Button id="selva_btn" draggable="true">
           Click me
         </Button>
-
-        <div id="terminal" ref={terminalRef}></div>
       </div>
+
+      <DynamicXtermTerminal />
     </section>
   );
 }
