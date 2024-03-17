ba84b790357c5f642a9f8072259ac6f9a41f4ecd
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-17 22:44:34 +0100

    added card

--- app/(routes)/(landing)/page.tsx
+++ app/(routes)/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import FeaturesSection from "@/app/(routes)/(landing)/_components/features-section";
 import HeroSection from "@/app/(routes)/(landing)/_components/hero-section";
+import { CardWithForm } from "@/components/cards/demo-card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Wireframe } from "@/components/ui/wireframe";
 
@@ -10,6 +11,14 @@ export default function Home() {
 
       <div className="flex justify-center space-x-4 animate-none">
         <Wireframe height="200" width="200" text="selva" />
+      </div>
+
+      <div>
+        <CardWithForm />
+      </div>
+
+      <div className="dark1">
+        <CardWithForm />
       </div>
     </main>
   );
