61959e8f6fd21592ee1d97b7c894df71991e4bd1
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-26 22:09:27 +0100

    remote mdx works

--- app/(routes)/(main)/selva/page.tsx
+++ app/(routes)/(main)/selva/page.tsx
@@ -1,15 +1,18 @@
-import FeaturesSection from "@/app/(routes)/(landing)/_components/features-section";
-import HeroSection from "@/app/(routes)/(landing)/_components/hero-section";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { serialize } from "next-mdx-remote/serialize";
 import { CardWithForm } from "@/components/cards/demo-card";
-import { Skeleton } from "@/components/ui/skeleton";
-import { Wireframe } from "@/components/ui/wireframe";
 
-export default function Home() {
+export default async function RemoteMdxPage() {
+  const components = { CardWithForm };
+
+  const source =
+    "### Selva Some **mdx** text, with a component <CardWithForm /> <CardWithForm />";
+  const res = await serialize(source);
+
   return (
-    <section id="main" className="flex min-h-screen flex-col items-center p-24">
-      <div className="dark-client">
-        <CardWithForm />
-      </div>
-    </section>
+    <div className="wrapper">
+      <MDXRemote source={source} components={components} />;
+      {/* <CardWithForm /> */}
+    </div>
   );
 }
