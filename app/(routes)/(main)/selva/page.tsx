46d5066a0d03dceddec6ece28475c533b237ea45
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-03-26 23:02:49 +0100

    styling works

--- app/(routes)/(main)/selva/page.tsx
+++ app/(routes)/(main)/selva/page.tsx
@@ -5,12 +5,11 @@ import { CardWithForm } from "@/components/cards/demo-card";
 export default async function RemoteMdxPage() {
   const components = { CardWithForm };
 
-  const source =
-    "### Selva Some **mdx** text, with a component <CardWithForm /> <CardWithForm />";
+  const source = "### Selva Some **mdx** text, with a component";
   const res = await serialize(source);
 
   return (
-    <div className="wrapper">
+    <div className="prose bg-white">
       <MDXRemote source={source} components={components} />;
       {/* <CardWithForm /> */}
     </div>
