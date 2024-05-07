14ef10460615d88697aecf4ba810033914057ec6
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-07 22:03:57 +0200

    fixed warning

--- components/component-overlay.tsx
+++ components/component-overlay.tsx
@@ -2,18 +2,6 @@
 
 import { ComponentOverlayProps } from "@/app/types/nav";
 import EditBoxSmall from "@/components/edit-boxes/edit-box-small";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { useEffect, useRef, useState } from "react";
 
 export default function ComponentOverlay({
@@ -24,19 +12,19 @@ export default function ComponentOverlay({
 
   const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        console.log("You clicked outside of me!");
-        setShowEditBox(false);
-      }
-    }
+  //   useEffect(() => {
+  //     function handleClickOutside(event: MouseEvent) {
+  //       if (ref.current && !ref.current.contains(event.target as Node)) {
+  //         console.log("You clicked outside of me!");
+  //         setShowEditBox(false);
+  //       }
+  //     }
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [ref]);
+  //     document.addEventListener("mousedown", handleClickOutside);
+  //     return () => {
+  //       document.removeEventListener("mousedown", handleClickOutside);
+  //     };
+  //   }, [ref]);
 
   return (
     <div
