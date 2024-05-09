f52b1def516edf93dcf8f0922a847c0dfb8e497f
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-09 20:04:05 +0200

    fixed warning

--- components/wasm-demo.tsx
+++ components/wasm-demo.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import * as wasm from "../public/wasm/index_bg.wasm";
 import { __wbg_set_wasm } from "../public/wasm/index_bg.js";
 __wbg_set_wasm(wasm);
@@ -6,8 +8,13 @@ import { parseSync, add } from "../public/wasm/index_bg.js";
 
 export async function WasmDemo({ number }: { number: number }) {
   const code = "let foo;export default async function Page(){}";
+
+  const startTime = performance.now();
   const result = parseSync(code, { sourceFilename: "test.tsx" });
+  const endTime = performance.now();
+
   console.log(result.program.body);
+  console.log("Execution time: ", endTime - startTime, "ms");
 
   return <>{add(number, number + 4)}</>;
 }
