f0491dec3e6a4a02d98032dcdb695d264891d041
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-10 16:09:16 +0200

    calling wasm func on btn click works

--- components/mouse-listener.tsx
+++ components/mouse-listener.tsx
@@ -1,19 +1,34 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { __wbg_set_wasm } from "../public/wasm/index_bg.js";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
 
-export function MouseListenerComponent({ number }: { number: number }) {
-  async function initializeWasm() {
-    const wasmModule = await import("../public/wasm/index_bg.wasm");
-    __wbg_set_wasm(wasmModule);
-    const { add, mouse_listener } = wasmModule;
-    mouse_listener();
-  }
+function MouseListenerComponent({ number }: { number: number }) {
+  const [add, setAdd] = useState<Function | null>(null);
 
   useEffect(() => {
+    async function initializeWasm() {
+      const wasmModule = await import("../public/wasm/index_bg.wasm");
+      __wbg_set_wasm(wasmModule);
+      const { add, mouse_listener } = wasmModule;
+      setAdd(() => add);
+    }
+
     initializeWasm();
   }, []);
 
-  return <>selv</>;
+  return (
+    <div>
+      <Button onClick={() => console.log(add(1, 2))}>Click me</Button>
+    </div>
+  );
 }
+
+const MouseListener = dynamic(() => Promise.resolve(MouseListenerComponent), {
+  // Ensure only client-side execution:
+  ssr: false,
+});
+
+export default MouseListener;
