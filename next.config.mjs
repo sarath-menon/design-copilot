eea73c2ca7e6bc307d74f8987fc89a113e5dfbc9
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-05-10 13:42:23 +0200

    added webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -7,19 +7,19 @@ const nextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx', 'md'],
     // Optionally, add any other Next.js config below
 
-    // webpack(config, { isServer, dev }) {
-    //     // Use the client static directory in the server bundle and prod mode
-    //     // Fixes `Error occurred prerendering page "/"`
-    //     config.output.webassemblyModuleFilename =
-    //         isServer && !dev
-    //             ? "../static/wasm/[modulehash].wasm"
-    //             : "static/wasm/[modulehash].wasm";
+    webpack(config, { isServer, dev }) {
+        // Use the client static directory in the server bundle and prod mode
+        // Fixes `Error occurred prerendering page "/"`
+        config.output.webassemblyModuleFilename =
+            isServer && !dev
+                ? "../static/wasm/[modulehash].wasm"
+                : "static/wasm/[modulehash].wasm";
 
-    //     // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    //     config.experiments = { ...config.experiments, asyncWebAssembly: true };
+        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+        config.experiments = { ...config.experiments, asyncWebAssembly: true };
 
-    //     return config;
-    // }
+        return config;
+    }
 }
 
 const withMDX = createMDX({
